d3523b2427cc4daae33900fc4090db846732791e
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 11:15:45 +0200

    Part 4: Handle  Custom delimiter in add function

--- FSharpTask/Functions.fs
+++ FSharpTask/Functions.fs
@@ -1,12 +1,20 @@
 ﻿module ConsoleApplication2.Functions
 open System
-open System
+open System.Text.RegularExpressions
 
 let add (s : String) : int =
     if s.Equals("") then
         0
     else
-        let numbers = s.Split [| ','; '\n' |]
+        //default values for input strings and delimiter
+        let mutable inputString = s
+        let mutable delimiter = ','
+        //check it the string starts with custom delimater
+        if Regex.Match(s, "^//.\n").Success then
+            delimiter <- s.[2]
+            inputString <- s.[4..]
+
+        let numbers = inputString.Split [| delimiter; '\n' |]
         //check that there are no two delimiters after each other
         if numbers |> Array.exists (fun num -> num.Equals("")) then
             printfn "Invalid string"
