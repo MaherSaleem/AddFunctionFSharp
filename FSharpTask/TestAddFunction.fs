0cb358faf0397157312f566a22c036d99e3bb9ff
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 14:04:01 +0200

    Part 8 + Part 9: Support multi delimiter Test Cases

--- FSharpTask/TestAddFunction.fs
+++ FSharpTask/TestAddFunction.fs
@@ -29,19 +29,19 @@ type TestAddFunction() =
         let s = "3,4"
         let expectedResult = 7
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.AddTwoNumbersMoreThanOneDigit15And13SeperatedByComma() =
         let s = "15,13"
         let expectedResult = 28
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.AddFiveNumberEachOfThemMoreThanOneDigit() =
         let s = "15,13,12,20,100"
         let expectedResult = 160
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.AddFourNumbersThatHaveNewLineInsideTheString() =
         let s = "15\n13,12\n20"
@@ -54,34 +54,46 @@ type TestAddFunction() =
         let s = "15\n,13,12\n20"
         let expectedResult = -1
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.AddThreeNumbersWithHashAsADelimiter() =
         let s = "//[#]\n15#13#12#20"
         let expectedResult = 60
         Assert.AreEqual(expectedResult, (add (s)))
-        
-        
-    //TODO this test case fails
+
+
     [<Test>]
     member this.AddThreeNumbersWithSemiColonAsADelimiterAndNewLines() =
         let s = "//;\n15;13;12\n20"
         let expectedResult = 60
         Assert.AreEqual(expectedResult, (add (s)))
+
     [<Test>]
     member this.IgnoreTwoNumbersThatAreGreaterThan1000() =
         let s = "200,100,1500,300000"
         let expectedResult = 300
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.TestMultiCharDelimiter2Chars() =
         let s = "//[##]\n1##2##3"
         let expectedResult = 6
         Assert.AreEqual(expectedResult, (add (s)))
-        
+
     [<Test>]
     member this.TestMultiCharDelimiter3Chars() =
         let s = "//[***]\n100***200***50***1500"
+        let expectedResult = 350
+        Assert.AreEqual(expectedResult, (add (s)))
+
+    [<Test>]
+    member this.MultiDelimiterSingleChar() =
+        let s = "//[*][#]\n100*200#50*1500"
+        let expectedResult = 350
+        Assert.AreEqual(expectedResult, (add (s)))
+
+    [<Test>]
+    member this.MultiDelimiterMultipleChars() =
+        let s = "//[***][##]\n100##200***50***1500"
         let expectedResult = 350
         Assert.AreEqual(expectedResult, (add (s)))
