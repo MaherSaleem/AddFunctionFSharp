00c330f3906ff681ff6e67b99e0caaae27e6d9b3
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 10:30:59 +0200

    Part 3: Support new line as a delimiters for the Add function

--- FSharpTask/Functions.fs
+++ FSharpTask/Functions.fs
@@ -5,7 +5,7 @@ let add (s : String) : int =
     if s.Equals("") then
         0
     else
-        let sum = s.Split [| ',' |] |> Array.map (fun num -> int (num)) |> Array.reduce (+)
+        let sum = s.Split [| ','; '\n' |] |> Array.map (fun num -> int (num)) |> Array.reduce (+)
         sum
 
 
