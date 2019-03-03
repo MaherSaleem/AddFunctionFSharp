e04167bc0edf97a7628414432bff22d52fc89ad9
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 14:19:25 +0200

    Commenting Add function

--- FSharpTask/Functions.fs
+++ FSharpTask/Functions.fs
@@ -2,6 +2,7 @@
 open System
 open System.Text.RegularExpressions
 
+
 let add (s : String) : int =
     if s.Equals("") then
         0
@@ -11,6 +12,7 @@ let add (s : String) : int =
         let mutable delimiter = ','
         //check it the string starts with custom delimater
 
+        //function to get the regex that matches multi-delimiter 
         let getRegexPattern : String =
             let numberOfOpenBrackets = s |> String.filter (fun c -> c = '[') |> String.length
             let mutable regexPattern = "^//"
@@ -18,14 +20,18 @@ let add (s : String) : int =
                 regexPattern <- regexPattern + "(\[[^][\n]*\])"
             regexPattern <- regexPattern + "\n(.*)"
             regexPattern
+            
+            
         let regexPattern = getRegexPattern
-
         let checkDelimiterRegex = Regex.Match(s, regexPattern)
+        //check if there are multi-delimiter
         if checkDelimiterRegex.Success then
-            let numberOfDelimiters = checkDelimiterRegex.Groups.Count - 2
+            let numberOfDelimiters = checkDelimiterRegex.Groups.Count - 2 // -2 since first one is the full match, and last one is the original stirng to be calcualted
             inputString <- checkDelimiterRegex.Groups.Item(numberOfDelimiters + 1).ToString()
             for i in [ 1..numberOfDelimiters ] do
+                //get the current delimiter
                 let multiCharDelimiter = checkDelimiterRegex.Groups.Item(i).ToString().Trim([|'['; ']'|])
+                //Replace all types of delimiters with ,
                 inputString <- inputString.Replace(multiCharDelimiter, ",")
 
         let numbers = inputString.Split [| delimiter; '\n' |]
@@ -35,9 +41,9 @@ let add (s : String) : int =
             -1
         else
             let sum = numbers
-                      |> Array.map (fun num -> int (num))
-                      |> Array.filter (fun num -> num <= 1000)
-                      |> Array.reduce (+)
+                      |> Array.map (fun num -> int (num)) // map string to integer
+                      |> Array.filter (fun num -> num <= 1000) // remove number that are greater than 1000
+                      |> Array.reduce (+) // sum all filtered data
 
             sum
 
