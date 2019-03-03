d1c5666d52083a6845b464efd7bdbc98674fe0e9
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 10:41:45 +0200

    Part 3: Handle Invalid strings in add function

--- FSharpTask/Functions.fs
+++ FSharpTask/Functions.fs
@@ -1,11 +1,18 @@
 ﻿module ConsoleApplication2.Functions
 open System
+open System
 
 let add (s : String) : int =
     if s.Equals("") then
         0
     else
-        let sum = s.Split [| ','; '\n' |] |> Array.map (fun num -> int (num)) |> Array.reduce (+)
-        sum
+        let numbers = s.Split [| ','; '\n' |]
+        //check that there are no two delimiters after each other
+        if numbers |> Array.exists (fun num -> num.Equals("")) then
+            printfn "Invalid string"
+            -1
+        else
+            let sum = numbers |> Array.map (fun num -> int (num)) |> Array.reduce (+)
+            sum
 
 
