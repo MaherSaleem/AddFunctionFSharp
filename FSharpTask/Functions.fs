d02422c7d0c23ec37d3f47293313c39a1a8ff5a8
Maher Khdeir <maher.khdeir@gmail.com>
2019-03-03 09:34:20 +0200

    Added implementation to pass sum of two "one digit" numbers separated by comma

--- FSharpTask/Functions.fs
+++ FSharpTask/Functions.fs
@@ -4,6 +4,9 @@ open System
 let add (s : String) : int =
     if s.Equals("") then
         0
+    elif s.Contains(",") then
+        let charToInt c = int c - int '0'
+        ((charToInt s.[0]) + (charToInt s.[2]))
     else
         int (s)
 
